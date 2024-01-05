76646dde8fef577475b8b2db71a86b4894b142a9
Jiwoo Lee <8075@refine.co.kr>
2024-01-05 17:57:20 +0900

    init project

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,9 @@
 import {createRouter, createWebHistory} from 'vue-router';
+import {routes} from "@/router/routes.js";
 
 const router = createRouter({
     history: createWebHistory('/infinitypo-fnd'),
+    routes
 });
 
 export default router;
