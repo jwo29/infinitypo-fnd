ab4291a77605599bd0aa9043e019944f4d3c1882
Jiwoo Lee <8075@refine.co.kr>
2024-01-05 17:28:58 +0900

    init project

---
+++ src/Home.vue
@@ -0,0 +1,13 @@
+<script setup>
+
+import ElementPlus from 'element-plus'
+
+</script>
+
+<template>
+
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/router/index.js
@@ -0,0 +1,7 @@
+import {createRouter, createWebHistory} from 'vue-router';
+
+const router = createRouter({
+    history: createWebHistory('/infinitypo-fnd'),
+});
+
+export default router;
